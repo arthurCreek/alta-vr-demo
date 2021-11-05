11f8da7e84d3d239be43e1575f03a9665eb81009
Arturo Arroyo <turo310@gmail.com>
2021-11-05 00:32:08 -0700

    Create header, footer, background, news container

---
+++ pages/components/Layout.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+import {useRouter} from 'next/router';
+import Footer from './Footer';
+import Header from './Header';
+import News from './News';
+// import Footer from './Footer';
+// import Showcase from './Showcase';
+// import styles from '@/styles/Layout.module.css';
+
+export interface Props {
+    title: string,
+    keywords: string,
+    description: string,
+    children: any
+}
+
+
+export default function Layout(props: Props) {
+    const router = useRouter();
+
+    return (
+        <div className="main-container">
+            <Head>
+                <title>{props.title}</title>
+                <meta name='decription' content={props.description} />
+                <meta name='keywords' content={props.keywords} />
+            </Head>
+
+        <Header/>
+
+        {/* {router.pathname === '/' && <Showcase />} */}
+
+        {/* <div className={styles.container}>
+            {props.children}
+        </div> */}
+
+        <News />
+
+        <Footer />
+        </div>
+    )
+}
+
+Layout.defaultProps = {
+    title: 'Tales of The Township | Latest News',
+    description: 'Latest News',
+    keywords: 'vr, tales of the township, altavr'
+}
---
+++ pages/components/News.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function News() {
+    return (
+        <div className="news-container w-7/12 h-1/2 mt-16 mx-auto">
+            <div className="news-header-container">
+                <h1>TALES OF THE TOWNSHIP</h1>
+                <p>UPDATES, NEWS AND DEV BLOGS</p>
+            </div>
+        </div>
+    )
+}
